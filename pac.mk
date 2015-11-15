9d98dec830e9f04f6e0e34a425cd2acfee676778
fizbanrapper <jeremiah.akers@outlook.com>
2015-11-15 14:04:38 -0500

    Move dependencies to one repo.  It's easier this way
    Change-Id: I8deb35fca93be52dd8f3c38cca90c24e0c5e79c9

--- pac.mk
+++ pac.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/pac/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := pac_m8
