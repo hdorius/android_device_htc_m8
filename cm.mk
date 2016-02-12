765fccbaf82e6204522fde60dc3aaabe92bc1aab
fizbanrapper <jeremiah.akers@outlook.com>
2016-02-11 22:09:54 -0500

    make beanstalk
    Change-Id: I7a984262284bbe2138417bb3be3297a93f7d2b8d

--- cm.mk
+++ cm.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := bs_m8
